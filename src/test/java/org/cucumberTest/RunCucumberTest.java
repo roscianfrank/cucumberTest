301b78b983c56bb9a56a7372382d2f0193236d4b
Roscian Frank <roscian.frank@gmail.com>
2023-04-21 15:44:10 +0100

    init

---
+++ src/test/java/org/cucumberTest/RunCucumberTest.java
@@ -0,0 +1,17 @@
+package org.cucumberTest;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("org/cucumberTest")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,json:target/cucumber-report/cucumber.json,html:target/cucumber-report/cucumber.html,rerun:target/cucumber-rerun, progress:target/cucumber-progress")
+@ConfigurationParameter(key = PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
+@ConfigurationParameter(key = PARALLEL_EXECUTION_ENABLED_PROPERTY_NAME, value = "true")
+public class RunCucumberTest {
+}
