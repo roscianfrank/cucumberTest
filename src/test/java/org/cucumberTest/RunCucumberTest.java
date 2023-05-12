983cfa33b6d9a1520b73d86a8869c25a24780d36
Roscian Frank <roscian.frank@gmail.com>
2023-05-12 19:03:59 +0100

    switching to stable channel

--- src/test/java/org/cucumberTest/RunCucumberTest.java
+++ src/test/java/org/cucumberTest/RunCucumberTest.java
@@ -13,6 +13,5 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,json:target/cucumber-report/cucumber.json,html:target/cucumber-report/cucumber.html,rerun:target/cucumber-rerun")
 @ConfigurationParameter(key = PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
 @ConfigurationParameter(key = PARALLEL_EXECUTION_ENABLED_PROPERTY_NAME, value = "false")
-@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @IGNORE")
 public class RunCucumberTest {
 }
